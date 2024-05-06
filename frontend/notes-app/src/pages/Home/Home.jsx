d0ea763a7bf664e936d4b39fd34e53dcf059aa71
RutvikMakvana4 <rutvikmakvana432@gmail.com>
2024-05-06 22:07:59 +0530

    add notes ui

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import NoteCard from "../../components/Cards/NoteCard";
+import { MdAdd } from "react-icons/md";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title="Meeting on 7th April"
+            date="3rd Apr 2024"
+            content="Meeting on 7th April Meeting on 7th April"
+            tags="#Meeting"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+
+      <button className="" onClick={() => {}}>
+        <MdAdd className="text-[32px] text-white" />
+      </button>
     </>
   );
 };
